00293a2d9980203b462e372a6883f46fa46497da
parbhez <masud.affb@gamil.com>
2020-04-02 21:20:30 +0600

    Login Registration

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+	if (!isset($_SESSION['user_id'])) {
+		header('location: login.php');	
+	}
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home page</title>
+	<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+	<style type="text/css">
+		.logout{
+			float: right;
+		}
+	</style>
+</head>
+<body>
+	<div class="container mt-5">
+		<div class="card">
+			<div class="card-header">
+				<h2>Home page <a href="logout.php" class="btn btn-outline-success logout">Logout</a></h2>
+			</div>
+			<div class="card-body">
+				<p>This is home page.This is home page.This is home page.This is home page.This is home page.This is home page.This is home page.This is home page.This is home page.This is home page.This is home page.This is home page.This is home page.This is home page.</p>
+
+
+				<p>This is home page.This is home page.This is home page.This is home page.This is home page.This is home page.This is home page.This is home page.This is home page.This is home page.This is home page.</p>
+
+				<p>This is home page.This is home page.This is home page.This is home page.This is home page.This is home page.This is home page.This is home page.This is home page.This is home page.This is home page.</p>
+
+				<p>This is home page.This is home page.This is home page.This is home page.This is home page.This is home page.This is home page.This is home page.This is home page.This is home page.This is home page.</p>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
